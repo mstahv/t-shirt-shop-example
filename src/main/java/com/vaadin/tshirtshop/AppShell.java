a60cc037504784b75afadec60d7cfbad11885c69
Matti Tahvonen <matti@vaadin.com>
2024-11-07 09:29:18 +0200

    theme & copilot

---
+++ src/main/java/com/vaadin/tshirtshop/AppShell.java
@@ -0,0 +1,8 @@
+package com.vaadin.tshirtshop;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+@Theme("my-theme")
+public class AppShell implements AppShellConfigurator {
+}
